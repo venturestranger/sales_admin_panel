0ae785622e903a3e0888b3fccebb98c5d6e06d2e
Bogdan Yakupov <yourinfinityuser@gmail.com>
2024-05-12 21:19:58 +0500

    added cheque post handler to receive cheques

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ class Config:
 	MONGO_URI = 'mongodb://127.0.0.1:27017/'
 	MONGO_DATABASE = 'sales_ai_v1'
 	MONGO_COLLECTION = 'sales'
-	
+	CHEQUE_HANDLER_PORT = 4322
--- utils.py
+++ utils.py
@@ -5,3 +5,37 @@ class Ad:
 		self.date = date
 		self.cost = cost
 		self.contact = contact
+
+
+def init_cheque_handler():
+	from fastapi import FastAPI, Response
+	from pydantic import BaseModel
+	from config import Config
+	import motor.motor_asyncio
+	import uvicorn
+
+	app = FastAPI()
+
+
+	class Cheque(BaseModel):
+		content: str
+		date: str
+		cost: str
+		contact: str
+		active: bool
+
+
+	@app.post('/sales/api/rest/v1/cheque')
+	async def post(cheque: Cheque):
+		client = motor.motor_asyncio.AsyncIOMotorClient(Config.MONGO_URI)
+		collection = client[Config.MONGO_DATABASE][Config.MONGO_COLLECTION]
+
+		await collection.insert_one(dict(cheque))
+
+		return Response(content='OK', status_code=200)
+	
+	uvicorn.run(app, host='0.0.0.0', port=Config.CHEQUE_HANDLER_PORT)
+
+
+if __name__ == "__main__":
+	init_cheque_handler()
